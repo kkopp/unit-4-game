bc787c6f6078289f14fc1fe399ea91d26cb80092
kkopp <kristina.kopp@gmail.com>
2018-10-12 23:10:00 -0600

    added a lot of css, still can't get js to work correctly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,57 @@
 $(document).ready(function() {
 
-// Generates random number to reach when you click on the gems
+//Global Variables
 var targetNumber = Math.floor((Math.random() * 100) + 19);
-
-// Writes the random number to the HTML element on the page
-$("#number-to-guess").text(targetNumber);
-
 var counter = 0;
 var wins = 0;
 var losses = 0;
+var gem = ["assets/images/PurpleGem.png", "assets/images/RedGem.png", "assets/images/SkyBlueGem.png", "assets/images/YellowGem.png", "assets/images/BlueGem.png", "assets/images/PinkGem.png", "assets/images/GreenGem.png"];
+
+
+
 
 //Creates random number for the Gem Values
 var numberOptions = function() {
     return Math.floor(Math.random() * 12);
 }
-var randoms = Array(4).fill(0).map(numberOptions);
-    console.log(randoms);
-
-// Array for Gem Photos
-var gem = ["assets/images/PurpleGem.png", "assets/images/RedGem.png", "assets/images/SkyBlueGem.png", "assets/images/YellowGem.png", "assets/images/BlueGem.png", "assets/images/PinkGem.png", "assets/images/GreenGem.png"];
-
+var randomGemValue = Array(4).fill(0).map(numberOptions);
+    console.log(randomGemValue);
 
 // Loop to add different gem photo on page, assign random value
-for (var i = 0; i < randoms.length; i++) {
+for (var i = 0; i < randomGemValue.length; i++) {
     var imageCrystal = $("<img>");
     var randomIndex = Math.floor(Math.random() * gem.length);
     imageCrystal.addClass("crystal-image");
     imageCrystal.attr("src", gem[randomIndex]);
-    imageCrystal.attr("data-crystalvalue", randoms[i]);
+    imageCrystal.attr("data-crystalvalue", randomGemValue[i]);
+    imageCrystal.attr("data-gemnumber", i);
     $("#crystals").append(imageCrystal);
 }
 
-function randomizeCrystal() {
-    for (var i = 0; i < 4; i++) {
-        randomIndex = Math.floor(Math.random() * gem.length);
-        $(".crystal-image").attr("src", gem[i]);
-    }
-};
+// function randomizeCrystal() {
+//     for (var i = 0; i < 4; i++) {
+//         randomIndex = Math.floor(Math.random() * gem.length);
+//         // console.log(gem.length);
+//         $(".crystal-image").attr("src", gem[randomIndex]);
+//     }
+// };
 
-function randomizeValues() {
-    for (var i = 0; i < 4; i++) {
-        var randoms = Array(4).fill(0).map(numberOptions);
-    console.log(randoms);
-        data-crystalvalue.attr(randoms[i]);
-    }
-}
+// function randomizeValues() {
+//     for (var i = 0; i < 4; i++) {
+//         var randomGemValue = Array(4).fill(0).map(numberOptions);
+//         data-crystalvalue.attr(randomGemValue[i]);
+//     }
+// }
 
 function reset() {
     counter = 0;
     $("#total-guessed").html(counter);
     targetNumber = Math.floor((Math.random() * 100) + 19);
     $("#number-to-guess").text(targetNumber);
-    randomizeCrystal();
-    randomizeValues();
+        for (var i = 0; i < 4; i++) {
+            $(".crystal-image").attr("src", gem[Math.floor(Math.random() * gem.length)]);
+        }
+    //randomizeValues();
     //randoms = Array(4).fill(0).map(numberOptions);
     //console.log(randoms);
     //$(".crystal-image").attr("data-crystalvalue", randoms[i]);
@@ -70,21 +69,21 @@ function reset() {
         if (counter === targetNumber) {
             alert("You Win");
             wins++;
-            $(".wins").html(wins);
+            $("#wins").html(wins);
             reset();
         } 
         
         else if (counter > targetNumber) {
             alert("You lose");
             losses++;
-            $(".losses").html(losses);
+            $("#losses").html(losses);
             reset();
         }
     });
 
 
-
-    $(".wins").html(wins);
-    $(".losses").html(losses)
+    $("#number-to-guess").text(targetNumber);
+    $("#wins").html(wins);
+    $("#losses").html(losses)
 
 });    
